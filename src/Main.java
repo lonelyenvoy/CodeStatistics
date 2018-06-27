71050d1d99b9c1a1e7ad2e1c3508b67b7ff1de66
lonelyenvoy <lonelyenvoy@gmail.com>
2018-06-27 14:04:29 +0800

    add Utils.getCodes

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import base.Functional;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -25,6 +24,23 @@ class Utils {
                 Integer::sum);
     }
 
+    @Pure
+    static List<String> getCodes(String path, String[] fileExtensions, boolean shuffle) {
+        List<File> files = traverseFolders(new File(path), Arrays.asList(fileExtensions));
+        if (shuffle) {
+            Collections.shuffle(files);
+        }
+        return files.stream().reduce(
+                new ArrayList<>(),
+                Functional.wrap((contents, file) -> {
+                    contents.addAll(Files.readAllLines(file.toPath()));
+                    return contents;
+                }),
+                (a, b) -> Stream.concat(a.stream(), b.stream()).collect(Collectors.toCollection(ArrayList::new))
+        );
+    }
+
+    @Pure
     private static List<File> traverseFolders(File root, List<String> validExtensions) {
         List<File> files = new ArrayList<>();
         if (root.isFile() && validExtensions.stream().anyMatch(x -> root.getName().endsWith(x))) {
